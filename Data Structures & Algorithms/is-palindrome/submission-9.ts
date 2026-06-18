dfd0eb80465a1385fa96e8caabff8ef324e5eba8
Thomas Medina <jushazen.dev@gmail.com>
2026-06-18 22:15:18 +0800

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.ts
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        let l = 0;
+        let r = s.length-1;
+
+        while (l < r) {
+            if (!this.isAlphaNum(s[l])) {
+                l++;
+                continue;
+            }
+
+            if (!this.isAlphaNum(s[r])) {
+                r--;
+                continue
+            }
+
+            if (s[l].toLowerCase() !== s[r].toLowerCase()) {
+                return false;
+            }
+
+            l++;
+            r--;
+        }
+        return true;
+
+    }
+
+    isAlphaNum(c: string): boolean {
+        return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'));
+    }
+}
