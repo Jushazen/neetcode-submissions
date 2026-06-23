01f87ddbcaeb1e5ebbc68580c75a4b8e6cd3a2a3
Thomas Medina <jushazen.dev@gmail.com>
2026-06-23 14:06:21 +0800

    Add: max-water-container - submission-9

---
+++ Data Structures & Algorithms/max-water-container/submission-9.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights: number[]): number {
+        let [max, l, r] = [0, 0, heights.length-1];
+
+        while (l < r) {
+            max = Math.max(Math.min(heights[l], heights[r]) * (r-l), max);
+            if (heights[l] <= heights[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+
+        return max;
+    }
+}
