fd3a49c03d587dd9bbda1bc52fd2433ec987d838
Thomas Medina <jushazen.dev@gmail.com>
2026-09-02 22:00:39 +0800

    Add: three-integer-sum - submission-15

---
+++ Data Structures & Algorithms/three-integer-sum/submission-15.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        let res = [];
+        nums.sort((a,b) => a - b);
+
+        for (let i = 0; i < nums.length-2; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
+
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+
+            let l = i+1, r = nums.length-1;
+
+            while (l < r) {
+                let sum = nums[i] + nums[l] + nums[r];
+
+                if (sum < 0) {
+                    l += 1;
+                } else if (sum > 0) {
+                    r -= 1;
+                } else {
+                    res.push([nums[i], nums[l], nums[r]]);
+                    l += 1;
+                    r -= 1;
+
+                    while (l < r && nums[l] == nums[l-1]) {
+                        l += 1;
+                    }
+                }
+            }
+        }
+        return res;
+
+    }
+}
