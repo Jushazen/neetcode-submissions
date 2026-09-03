5d45db01a93595ea04a2ed8b4a08480e61037e3a
Thomas Medina <jushazen.dev@gmail.com>
2026-09-03 21:32:55 +0800

    Add: max-water-container - submission-10

---
+++ Data Structures & Algorithms/max-water-container/submission-10.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let l = 0, r = heights.length-1;
+        let res = 0;
+        while (l < r) {
+            res = Math.max(res, Math.min(heights[l], heights[r]) * (r-l));
+            if (heights[l] < heights[r]) {
+                l += 1;
+            } else {
+                r -= 1;
+            }
+        }
+
+        return res;
+
+    }
+}
